4b92727dbd26fc51e3e95b9ee05495a09ab20c79
Simon M <simon.marache@gmail.com>
2014-09-27 14:15:49 +0000

    Chrono now use minified.js

--- chrono.js
+++ chrono.js
@@ -1,8 +1,46 @@
+var MINI = require('minified');
+var _=MINI._, $=MINI.$, $$=MINI.$$, EE=MINI.EE, HTML=MINI.HTML;
+
+
+// Date() at the beginning of the measured time
 var g_origin = null;
+
+// Date() since last Step
 var g_lastStep =null
-var entries = document.getElementById("entries");
-var counter = document.getElementById("counter");
+
+// Id of the window.setInterval Call
 var g_idInterval;
+
+var entries;
+var counter;
+
+
+// Beginning of program, set variables and Event handling
+$( function()
+{
+    entries = $$("#entries");
+    counter = $$("#counter");
+     
+    $('#step').on('click', step);
+    $('#reset').on('click', reset);
+    
+    // TODO: Refactor with a real object Timer
+    $('#start').on('click',
+        function()
+        {
+            if(g_idInterval === undefined)
+            {
+                start();
+            }
+            else
+            {
+                step(); // Show the last time
+                stop();
+            }
+        }
+    );
+});
+
 
 function fmtTime(t)
 {
@@ -14,19 +52,30 @@ function start()
     g_origin = new Date();
     g_lastStep = g_origin;
     entries.value += "" + fmtDate(g_origin) + "\n"
-    update()
     if(g_idInterval === undefined)
     {
         g_idInterval = window.setInterval(update, 200);
     }
+    $('#start').fill("Stop");
+    update()
+}
 
+function stop() 
+{
+    window.clearInterval(g_idInterval);
+    g_lastStep = undefined;
+    g_origin = undefined;
+    g_idInterval = undefined;
+    $('#start').fill("Start");
+    update();
 }
 
 function update()
 {
-    if(g_origin !== null)
+    var diff;
+    if(g_origin !== undefined)
     {
-        var diff = new Date() - g_origin;
+        diff = new Date() - g_origin;
         counter.firstChild.nodeValue = fmtTime(diff);
     }
     else
@@ -45,7 +94,7 @@ function fmtDate(d) { return pad(d.getHours(),2) + ":" + pad(d.getMinutes(),2) +
 
 function step()
 {
-    if(g_origin !== null)
+    if(g_origin !== undefined)
     {
         var newStep = new Date()
         entries.value += "" + fmtDate(newStep) + "; " + fmtTime(newStep -  g_origin)
@@ -57,11 +106,7 @@ function step()
 
 function reset()
 {
-    g_origin = null;
+    stop();
     entries.value = "";
-    window.clearInterval(g_idInterval);
-    g_idInterval = undefined;
-    
-    update();
 }      
 
