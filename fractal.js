eecdf91acaf7385d77bbf3c744e7bf94bc63a136
Simon M <simon.marache@gmail.com>
2014-09-30 17:46:22 +0000

    Refactoring: webGl object now has own class and file. Minor changes to the interface (brightness/contrast, fractal in log)

--- fractal.js
+++ fractal.js
@@ -1,12 +1,14 @@
+var WebGlComponent = WebGlComponent || {};
+
 var MINI = require('minified');
 var _=MINI._, $=MINI.$, $$=MINI.$$, EE=MINI.EE, HTML=MINI.HTML;
 
 
 var g_zoom
-var g_program;
 var g_defaultPosition;
 var g_position;
 var g_brightness;
+var g_contrast;
 var g_glContext;
 var g_dragOrigin;
 var g_canvas;
@@ -16,6 +18,32 @@ var g_animTime;
 $(function()
 {
 	
+
+var bindValues = function (glObject, glProgram)
+{
+// look up where the vertex data needs to go.
+		var cJulia = glObject.getUniformLocation(glProgram, "u_cJulia");
+		var move = glObject.getUniformLocation(glProgram, "u_fractalPosition");
+		var zoom = glObject.getUniformLocation(glProgram, "u_fractalZoom");
+		var brightness = glObject.getUniformLocation(glProgram, "u_brightness");
+		var contrast = glObject.getUniformLocation(glProgram, "u_contrast");
+		var highQuality = glObject.getUniformLocation(glProgram, "u_highQuality");
+		var qual = ($('#highQuality').get('checked') === true ? 1 : 0);
+		glObject.uniform2f(cJulia, g_cJulia[0], g_cJulia[1]);
+		glObject.uniform1f(zoom, g_zoom);
+		glObject.uniform1i(highQuality, qual );
+		glObject.uniform2f(move, g_position[0] + 0.5 * glObject.drawingBufferWidth / g_zoom, g_position[1] + 0.5 * glObject.drawingBufferHeight / g_zoom);
+		glObject.uniform1f(brightness, g_brightness);
+		glObject.uniform1f(contrast, g_contrast);
+};
+
+
+var g_gl = new WebGlComponent('#fractalCanvas', '#vertexShader', '#fractal', log);
+
+function drawFractal()
+{
+	g_gl.triggerDraw(bindValues);
+}
 
 // Get a context from our canvas object with id = "fractalCanvas".
 g_canvas = $("#fractalCanvas");
@@ -28,8 +56,6 @@ g_zoom = 256.0;
 // center fractal on canvas
 g_defaultPosition = [0, 0];
 g_position = g_defaultPosition.slice(0);
-
-
 
 
 // TODO: Refactoring, refactoring, refactoring !!!!
@@ -48,35 +74,31 @@ $("#logger").hide();
 
 function log(msg) { $('#logger').add(msg + '\n'); }
 
-function resizeCanvas(gl, newSize)
-{
-	log("Resize webgl canvas to " + newSize.x + ", " + newSize.y);
-	g_canvas.set('width', newSize.x);
-	g_canvas.set('height', newSize.y);
-	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-	drawFractal(g_glContext);
-}
+
 
 $('#small').on('click', function()
 {
-	resizeCanvas(g_glContext, {x:256, y:256});
+	g_gl.resizeCanvas({x:256, y:256});
+	drawFractal();
 });
 
 $('#medium').on('click', function()
 {
-	resizeCanvas(g_glContext, {x:512, y:512});
+	g_gl.resizeCanvas({x:512, y:512});
+	drawFractal();
 });
 
 $('#maximum').on('click', function()
 {
-	resizeCanvas(g_glContext, {x:screen.availWidth, y:screen.availHeight});
+	g_gl.resizeCanvas({x:screen.availWidth, y:screen.availHeight});
+	drawFractal();
 });
 
 $('#reset').on('click', function()
 {
 	$('#logger').fill();
 	g_position = g_defaultPosition.slice(0);
-	drawFractal(g_glContext);
+	drawFractal();
 });
 
 $('#animate').on('click', function()
@@ -86,7 +108,7 @@ $('#animate').on('click', function()
 
 $('#fullscreen').on('click', function()
 {
-	resizeCanvas(g_glContext, {x:screen.availWidth, y:screen.availHeight});
+	g_gl.resizeCanvas({x:screen.availWidth, y:screen.availHeight});
 	// From: https://developer.mozilla.org/fr/docs/Web/Guide/DOM/Using_full_screen_mode
 	var canvas =$$("#fractalCanvas"); 
 	if (canvas.requestFullscreen) {
@@ -116,7 +138,7 @@ function animationIteration()
   //	g_brightnessControl.changeValue(val_brght);
 	
 	g_animTime += 1;
-	drawFractal(g_glContext);
+	drawFractal();
 }
 
 function Control(id, callBackList, min, max, gl)
@@ -160,7 +182,7 @@ Control.prototype.changeValue = function(value, shouldNormalize)
 	this.refTxt.set('value', normValue.toFixed(2));
 	this.ref.set('value', sliderValue);
 	_.each(this.callBackList, function(item) { item(normValue) });
-	drawFractal(this.glContext);
+	drawFractal();
 };
 
 
@@ -187,7 +209,7 @@ g_canvas.on('wheel', function(wheelEvt)
 	{
 		g_zoom /= 1.1;
 	}
-	drawFractal(g_glContext);
+	drawFractal();
 });
 
 g_canvas.on('mousemove', function(a)
@@ -197,185 +219,28 @@ g_canvas.on('mousemove', function(a)
 		g_position = g_dragOrigin.orig_pos.slice(0);
 		g_position[0] += (a.clientX - g_dragOrigin.x)/g_zoom;
 		g_position[1] -= (a.clientY - g_dragOrigin.y)/g_zoom;
-		drawFractal(g_glContext);
+		drawFractal();
 	}
 });
 
 
-
-function loadProgram(gl, vertexShader, fragmentShader)
-{
-  // create a progam object
-  var program = gl.createProgram();
-
-  // attach the two shaders 
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-
-  // link everything 
-  gl.linkProgram(program);
-
-  // Check the link status
-  var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (!linked) {
-
-    // An error occurred while linking
-    var lastError = gl.getProgramInfoLog(program);
-    log("Error in program linking:" + lastError);
-
-    gl.deleteProgram(program);
-    return null;
-  }
-
-  // if all is well, return the program object
-  return program;
-}
-
-
-// Loads a shader from a script tag
-// Parameters:
-//   WebGL context
-//   id of script element containing the shader to load
-function getShader(gl, id) {
-  var shaderScript = $$(id);
-
-  // error - element with supplied id couldn't be retrieved
-  if (!shaderScript) {
-    log("couldn't find " + id);
-  }
-
-  // If successful, build a string representing the shader source
-  var str = "";
-  var k = shaderScript.firstChild;
-  while (k) {
-    if (k.nodeType == 3) {
-      str += k.textContent;
-    }
-    k = k.nextSibling;
-  }
-
-  var shader;
-
-  // Create shaders based on the type we set
-  //   note: these types are commonly used, but not required
-  if (shaderScript.type == "x-shader/x-fragment") {
-    shader = gl.createShader(gl.FRAGMENT_SHADER);
-  } else if (shaderScript.type == "x-shader/x-vertex") {
-    shader = gl.createShader(gl.VERTEX_SHADER);
-  } else {
-	log("unknown type for " + id);
-    return null;
-  }
-
-  gl.shaderSource(shader, str);
-  gl.compileShader(shader);
-
-  // Check the compile status, return an error if failed
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    log("Error compiling shader " + id + ": " + gl.getShaderInfoLog(shader));
-    return null;
-  }
-
-  return shader;
-}
-
-
-function initFractal()
-{
-	try {
-		// minifiedList[0] to get access to the raw DOM Object
-		g_glContext = g_canvas[0].getContext("webgl") || g_canvas[0].getContext("experimental-webgl");
-	}
-	catch (e) {
-		log("canvas getContext fail");
-	}
-
-	try
-	{
-		// setup a GLSL program
-		var vertexShader = getShader(g_glContext, "#vertexShader");
-		var fragmentShader = getShader(g_glContext, "#fractal");
-		g_program = loadProgram(g_glContext, vertexShader, fragmentShader);
-	}
-	catch (e) {
-	// Display the fail on the screen if the shaders/program fail.
-	log('initFractal fail: ' + e.message);
-	}
-}
-
-
-function drawFractal(gl)
-{
-	var a = new Date()
-	try
-	{
-		gl.useProgram(g_program);
-
-		// look up where the vertex data needs to go.
-		var positionLocation = gl.getAttribLocation(g_program, "a_position");
-		var cJulia = gl.getUniformLocation(g_program, "u_cJulia");
-		var move = gl.getUniformLocation(g_program, "u_fractalPosition");
-		var zoom = gl.getUniformLocation(g_program, "u_fractalZoom");
-		var brightness = gl.getUniformLocation(g_program, "u_brightness");
-		var highQuality = gl.getUniformLocation(g_program, "u_highQuality");
-		var qual = ($('#highQuality').get('checked') === true ? 1 : 0);
-		gl.uniform2f(cJulia, g_cJulia[0], g_cJulia[1]);
-		gl.uniform1f(zoom, g_zoom);
-		gl.uniform1i(highQuality, qual );
-		gl.uniform2f(move, g_position[0] + 0.5 * gl.drawingBufferWidth / g_zoom, g_position[1] + 0.5 * gl.drawingBufferHeight / g_zoom);
-		gl.uniform1f(brightness, g_brightness);
-			
-			
-			
-		// Create a buffer and put a single clipspace rectangle in
-		// it (2 triangles)
-		var buffer = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-		gl.bufferData(
-			gl.ARRAY_BUFFER, 
-			new Float32Array([
-				-1.0, -1.0, 
-				 1.0, -1.0, 
-				-1.0,  1.0, 
-				-1.0,  1.0, 
-				 1.0, -1.0, 
-				 1.0,  1.0]), 
-			gl.STATIC_DRAW);
-		gl.enableVertexAttribArray(positionLocation);
-		gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-
-		// draw
-		gl.drawArrays(gl.TRIANGLES, 0, 6);
-		gl.finish();
-	}
-	catch (e) {
-		log('drawFractal fail: ' + e.message);
-	}
-	log("time: " + (new Date() - a) + " ms. position: " + g_position)
-
-}
-
-
 log("Starting");
 
-initFractal();
+g_gl.resizeCanvas({x:window.screen.availWidth - 20, y:512});
 
-resizeCanvas(g_glContext, {x:window.screen.availWidth - 20, y:512});
-
-drawFractal(g_glContext);
+drawFractal();
 
 
 // TODO: make object
-$('#highQuality').on('change', function() { drawFractal(g_glContext); } ) ;
+$('#highQuality').on('change', function() { drawFractal(); } ) ;
 
 var g_c0 = new Control('#c0', [ function(value) { g_cJulia[0] = value } ], -1, 1, g_glContext);
 var g_c1 = new Control('#c1', [ function(value) { g_cJulia[1] = value } ], -1, 1, g_glContext);
-var g_brightnessControl = new Control('#brightness', [ function(value) { g_brightness = value } ], 1, 300, g_glContext);
+var g_brightnessControl = new Control('#brightness', [ function(value) { g_brightness = value } ], -1, 1, g_glContext);
+var g_contrastControl = new Control('#contrast', [ function(value) { g_contrast = value } ], 0, 10, g_glContext);
 
 g_c0.changeValue(-0.76);
 g_c1.changeValue(-0.08);
-g_brightnessControl.changeValue(4);
-
-
-
+g_brightnessControl.changeValue(-0.6);
+g_contrastControl.changeValue(0.3)
 });
---
+++ webgl.js
@@ -0,0 +1,150 @@
+function WebGlComponent(idCanvas, idVertexShader, idFragmentShader, log)
+{
+  this.logFunction = log;
+  this.canvas = $$(idCanvas);
+	try {
+		// minifiedList[0] to get access to the raw DOM Object
+		this.glContext = this.canvas.getContext("webgl") || this.canvas.getContext("experimental-webgl");
+	}
+	catch (e) {
+		this.logFunction("canvas getContext fail");
+	}
+	try
+	{
+		// setup a GLSL program
+		this.vertexShader = this.getShaderFromDOM($$(idVertexShader));
+		this.fragmentShader = this.getShaderFromDOM($$(idFragmentShader));
+		this.loadProgram(this.glContext, this.vertexShader, this.fragmentShader);
+	}
+	catch (e) {
+	// Display the fail on the screen if the shaders/program fail.
+	this.logFunction('initFractal fail: ' + e.message);
+	}
+}
+
+
+// Draw the WebGL 3D Data. 
+// - registerVariables: function(WebGlComponent) : make link between data and shaders (uniforms, attribute...)
+WebGlComponent.prototype.triggerDraw = function(registerVariables)
+{
+	var a = new Date();
+	try
+	{
+		this.glContext.useProgram(this.glProgram);
+
+    registerVariables(this.glContext, this.glProgram);
+
+		// Create a buffer and put a single clipspace rectangle in
+		// it (2 triangles)
+		var buffer = this.glContext.createBuffer();
+		this.glContext.bindBuffer(this.glContext.ARRAY_BUFFER, buffer);
+		this.glContext.bufferData(
+			this.glContext.ARRAY_BUFFER, 
+			new Float32Array([
+				-1.0, -1.0, 
+				 1.0, -1.0, 
+				-1.0,  1.0, 
+				-1.0,  1.0, 
+				 1.0, -1.0, 
+				 1.0,  1.0]), 
+			this.glContext.STATIC_DRAW);
+			
+		var positionLocation = this.glContext.getAttribLocation(this.glProgram, "a_position");
+		this.glContext.enableVertexAttribArray(positionLocation);
+		this.glContext.vertexAttribPointer(positionLocation, 2, this.glContext.FLOAT, false, 0, 0);
+
+		// draw
+		this.glContext.drawArrays(this.glContext.TRIANGLES, 0, 6);
+		this.glContext.finish();
+	}
+	catch (e) {
+		this.logFunction('drawFractal fail: ' + e.message);
+	}
+	return (new Date() - a);
+};
+
+
+// get compiled code from this.vertexShader and this.fragmentShader, and link them in a program.
+// Return true on success, false on error.
+WebGlComponent.prototype.loadProgram = function()
+{
+  // create a progam object
+  this.glProgram = this.glContext.createProgram();
+
+  // attach the two shaders 
+  this.glContext.attachShader(this.glProgram, this.vertexShader);
+  this.glContext.attachShader(this.glProgram, this.fragmentShader);
+
+  // link everything 
+  this.glContext.linkProgram(this.glProgram);
+
+  // Check the link status
+  var linked = this.glContext.getProgramParameter(this.glProgram, this.glContext.LINK_STATUS);
+  if (!linked) {
+
+    // An error occurred while linking
+    var lastError = this.glContext.getProgramInfoLog(this.glProgram);
+    this.logFunction("Error in program linking:" + lastError);
+
+    this.glContext.deleteProgram(this.glProgram);
+    return false;
+  }
+  return true;
+};
+
+
+// Loads a shader from a script tag
+// Parameters:
+//   WebGL context
+//   id of script element containing the shader to load
+WebGlComponent.prototype.getShaderFromDOM = function(id) {
+  var shaderScript = $$(id);
+
+  // error - element with supplied id couldn't be retrieved
+  if (!shaderScript) {
+    this.logFunction("couldn't find " + id);
+  }
+
+  // If successful, build a string representing the shader source
+  var str = "";
+  var k = shaderScript.firstChild;
+  while (k) {
+    if (k.nodeType == 3) {
+      str += k.textContent;
+    }
+    k = k.nextSibling;
+  }
+
+  var shader;
+
+  // Create shaders based on the type we set
+  //   note: these types are commonly used, but not required
+  if (shaderScript.type == "x-shader/x-fragment") {
+    shader = this.glContext.createShader(this.glContext.FRAGMENT_SHADER);
+  } else if (shaderScript.type == "x-shader/x-vertex") {
+    shader = this.glContext.createShader(this.glContext.VERTEX_SHADER);
+  } else {
+  	this.logFunction("unknown type for " + id);
+    return null;
+  }
+
+  this.glContext.shaderSource(shader, str);
+  this.glContext.compileShader(shader);
+
+  // Check the compile status, return an error if failed
+  if (!this.glContext.getShaderParameter(shader, this.glContext.COMPILE_STATUS)) {
+    this.logFunction("Error compiling shader " + id + ": " + this.glContext.getShaderInfoLog(shader));
+    return null;
+  }
+
+  return shader;
+};
+
+
+WebGlComponent.prototype.resizeCanvas = function(newSize)
+{
+	this.logFunction("Resize webgl canvas to " + newSize.x + ", " + newSize.y);
+	this.canvas.width = newSize.x;
+	this.canvas.height = newSize.y;
+	this.glContext.viewport(0, 0, newSize.x, newSize.y);
+}
